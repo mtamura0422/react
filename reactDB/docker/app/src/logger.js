cf8a7b224978ac51c813ff799f80a0330973ad7f
mtamura0422 <mtamura0422@gmail.com>
2024-09-07 17:12:49 +0900

    Delete reactDB directory

--- reactDB/docker/app/src/logger.js
+++
@@ -1,9 +0,0 @@
-var log4js = require('log4js');
-var logger = exports = module.exports = {};
-log4js.configure({
-  appenders: { debug: { type: 'file', filename: 'logs/debug.log' } },
-  categories: { default: { appenders: ['debug'], level: 'debug' } }
-});
-
-
-logger.debug = log4js.getLogger('debug');
--- reactDB/docker/app/src/src/firebase/config.js
+++
@@ -1,8 +0,0 @@
-export const config = {
-  apiKey: "AIzaSyDIk_55NuVi3SEAN9B_N5h2PClO8mSQSTI",
-  authDomain: "app-labo-02.firebaseapp.com",
-  databaseURL: "https://app-labo-02.firebaseio.com",
-  projectId: "app-labo-02",
-  storageBucket: "app-labo-02.appspot.com",
-  messagingSenderId: "344133423090"
-};
